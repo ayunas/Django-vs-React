8f7528bd4e9be6a0d1986a0296543e5b83740c67
Devin Warrick <dev.warr@gmail.com>
2020-01-03 07:44:00 -0600

    Run migrations, add useNullAsDefault in knexfile

--- React-Node/node/data/migrations/20200103073731_init.js
+++ React-Node/node/data/migrations/20200103073731_init.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("files", tbl => {
+      tbl.increments();
+      tbl.string("framework")
+        .notNullable();
+      tbl.string("folder")
+        .notNullable();
+      tbl.string("file")
+        .notNullable();
+  })
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("files")
 };
--- React-Node/node/knexfile.js
+++ React-Node/node/knexfile.js
@@ -7,6 +7,7 @@ module.exports = {
     connection: {
       filename: './dev.sqlite3'
     },
+    useNullAsDefault: true,
     migrations: {
       directory: "./data/migrations"
     },
