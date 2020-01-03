241b08099bb7ce4ae1edd754b0987a549f7a875b
Devin Warrick <dev.warr@gmail.com>
2020-01-03 06:32:46 -0600

    Create Files Model

--- Django/django_structure/models.py
+++ Django/django_structure/models.py
@@ -7,3 +7,11 @@ class Django_Structure(models.Model):
 
     def __str__(self):
         return self.title
+
+class Files(models.Model):
+    framework = models.CharField()
+    folder = models.CharField()
+    file = models.CharField() 
+
+    def __str__(self):
+        return self.file
