66a7275ff0bd6ba24df6420a9cf7e16c85039cb9
Devin Warrick <dev.warr@gmail.com>
2020-01-03 07:33:20 -0600

    Updates settings and url to display html templates

--- Django/djangoreact/urls.py
+++ Django/djangoreact/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django_structure.views import home_page
+
 urlpatterns = [
+    path("", home_page, name="home"),
     path('admin/', admin.site.urls),
 ]
